426fceada5e3907422b245e3f5d6e5179f32e8df
Jacob Hoskins <jacobhoskins997@gmail.com>
2023-09-25 20:07:20 -0400

    base config update

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const config = {
   authRequired: false,
   auth0Logout: true,
   secret: "a long, randomly-generated string stored in env",
-  baseURL: "iet.onrender.com",
+  baseURL: "https://www.iet.onrender.com",
   clientID: "DbIQU4pabkjxucqerUJpqhfPKh0Oxt0N",
   issuerBaseURL: "https://dev-2nzpe3olofuxkog8.us.auth0.com",
 };
